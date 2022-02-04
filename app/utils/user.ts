16f51a3d650c86f6eca719214cd8194d15cd3c96
M Haidar Hanif <git@mhaidarhanif.com>
2022-02-05 02:29:28 +0700

    feat: util get user handle

--- app/utils/user.ts
+++ app/utils/user.ts
@@ -2,3 +2,5 @@ import type { TUser } from '~/types'
 
 export const getUserName = (user: TUser) =>
   `${user?.firstName} ${user?.lastName}`
+
+export const getUserHandle = (user: TUser) => user?.handle
