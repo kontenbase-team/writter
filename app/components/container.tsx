3ecfbe10131522cc50ea3d3181d2c5d2c70d003f
M Haidar Hanif <git@mhaidarhanif.com>
2022-02-05 03:21:47 +0700

    feat: container navigate back button

--- app/components/container.tsx
+++ app/components/container.tsx
@@ -1,29 +1,57 @@
+import { ArrowBackIcon } from '@chakra-ui/icons'
 import {
-  Box,
+  Button,
   Container as ChakraContainer,
   Divider,
   Heading,
+  HStack,
+  IconButton,
 } from '@chakra-ui/react'
 import { FunctionComponent } from 'react'
+import { useNavigate } from 'remix'
 
 interface ContainerProps {
+  isBackDisabled?: boolean
   headingText?: string
   children: React.ReactNode
 }
 
 export const Container: FunctionComponent<ContainerProps> = ({
+  isBackDisabled,
   headingText,
   children,
-}) => (
-  <ChakraContainer px={0} borderColor="gray.600" borderWidth={1} borderTop={0}>
-    <Box as="header" p={5}>
-      <Heading as="h1" size="lg">
-        {headingText || 'Writter'}
-      </Heading>
-    </Box>
+}) => {
+  const navigate = useNavigate()
 
-    <Divider />
+  const navigateBack = () => {
+    navigate(-1)
+  }
 
-    {children}
-  </ChakraContainer>
-)
+  return (
+    <ChakraContainer
+      px={0}
+      borderColor="gray.600"
+      borderWidth={1}
+      borderTop={0}
+    >
+      <HStack as="header" p={5} spacing={5}>
+        {!isBackDisabled && (
+          <IconButton
+            onClick={navigateBack}
+            aria-label="Navigate Back"
+            icon={<ArrowBackIcon />}
+            borderRadius="full"
+            variant="ghost"
+          />
+        )}
+        <Heading as="h1" size="lg">
+          {headingText || 'Writter'}
+        </Heading>
+      </HStack>
+
+      <Divider />
+
+      {children}
+    </ChakraContainer>
+  )
+}
