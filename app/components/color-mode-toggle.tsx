c04dc6c1597d4aaa7328be4eb9d6833b674f5c08
M Haidar Hanif <git@mhaidarhanif.com>
2022-02-05 04:59:14 +0700

    style: tooltip color mode toggle

--- app/components/color-mode-toggle.tsx
+++ app/components/color-mode-toggle.tsx
@@ -1,19 +1,23 @@
 import { MoonIcon, SunIcon } from '@chakra-ui/icons'
-import { IconButton, useColorMode } from '@chakra-ui/react'
+import { IconButton, Tooltip, useColorMode } from '@chakra-ui/react'
 import { FunctionComponent } from 'react'
 
 interface ColorModeToggleProps {}
 
 export const ColorModeToggle: FunctionComponent<ColorModeToggleProps> = () => {
   const { colorMode, toggleColorMode } = useColorMode()
+  const colorModeTarget = colorMode === 'light' ? 'dark' : 'light'
+  const label = `Toggle to ${colorModeTarget} mode`
 
   return (
-    <IconButton
-      onClick={toggleColorMode}
-      aria-label="Toggle color mode"
-      icon={colorMode === 'light' ? <MoonIcon /> : <SunIcon />}
-      variant="ghost"
-      borderRadius="full"
-    />
+    <Tooltip label={label}>
+      <IconButton
+        onClick={toggleColorMode}
+        aria-label={label}
+        icon={colorMode === 'light' ? <MoonIcon /> : <SunIcon />}
+        variant="ghost"
+        borderRadius="full"
+      />
+    </Tooltip>
   )
 }
