b5983d96f18d0a109f0dfa10632c2eacbfb1ed1a
M Haidar Hanif <git@mhaidarhanif.com>
2022-02-05 01:12:00 +0700

    feat: link of wreet card

--- app/features/wreets/card.tsx
+++ app/features/wreets/card.tsx
@@ -1,5 +1,6 @@
 import { chakra, Box, Text, Avatar, HStack } from '@chakra-ui/react'
 import { FunctionComponent } from 'react'
+import { Link as RemixLink } from 'remix'
 
 import { getRelativeDate } from '~/lib'
 import type { TWreet } from '~/types'
@@ -13,25 +14,27 @@ export const WreetCard: FunctionComponent<WreetCardProps> = ({ wreet }) => {
   const user = wreet?.createdBy
 
   return (
-    <Box p={5}>
-      <HStack spacing={5} align="flex-start">
-        <Avatar name={getUserName(user)} />
-        <Box>
-          <HStack>
-            <chakra.span size="sm" fontWeight="bold">
-              {getUserName(user)}
-            </chakra.span>
-            <chakra.span size="sm" color="gray.500">
-              <span>@{wreet?.createdBy?.handle}</span>
-              <span> · </span>
-              <span>{getRelativeDate(wreet?.createdAt)}</span>
-            </chakra.span>
-          </HStack>
-          <Text mt={1}>{wreet?.content}</Text>
-          <Text mt={1}>{wreet?.content}</Text>
-          <Text mt={1}>{wreet?.content}</Text>
-        </Box>
-      </HStack>
-    </Box>
+    <RemixLink to={`/${user?.handle}/${wreet?._id}`}>
+      <Box p={5}>
+        <HStack spacing={5} align="flex-start">
+          <Avatar name={getUserName(user)} />
+          <Box>
+            <HStack>
+              <chakra.span size="sm" fontWeight="bold">
+                {getUserName(user)}
+              </chakra.span>
+              <chakra.span size="sm" color="gray.500">
+                <span>@{user?.handle}</span>
+                <span> · </span>
+                <span>{getRelativeDate(wreet?.createdAt)}</span>
+              </chakra.span>
+            </HStack>
+            <Text mt={1}>{wreet?.content}</Text>
+            <Text mt={1}>{wreet?.content}</Text>
+            <Text mt={1}>{wreet?.content}</Text>
+          </Box>
+        </HStack>
+      </Box>
+    </RemixLink>
   )
 }
