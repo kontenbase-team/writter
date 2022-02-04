d1a7aa9a1412ce2f39b3795d36ad6399949c26fa
M Haidar Hanif <git@mhaidarhanif.com>
2022-02-05 02:56:58 +0700

    feat: util get wreet url

--- app/utils/wreet.ts
+++ app/utils/wreet.ts
@@ -1,1 +1,8 @@
+import { getUserHandle } from '.'
+
+import { TWreet } from '~/types'
+
+export const getWreetURL = (wreet: TWreet) =>
+  `/${getUserHandle(wreet?.createdBy)}/${wreet?._id}`
+
 export const getTrimmedWreet = (content: string) => content.substring(0, 42)
