25146d6d4ee8af5ff7c6b34e4c55b797f3caa2e6
M Haidar Hanif <git@mhaidarhanif.com>
2022-02-05 04:28:58 +0700

    feat+refactor: navigation sidebar in container

--- app/components/container.tsx
+++ app/components/container.tsx
@@ -1,14 +1,24 @@
-import { ArrowBackIcon } from '@chakra-ui/icons'
 import {
-  Button,
+  ArrowBackIcon,
+  EditIcon,
+  SettingsIcon,
+  TimeIcon,
+} from '@chakra-ui/icons'
+import {
+  Box,
+  Image,
   Container as ChakraContainer,
   Divider,
   Heading,
   HStack,
   IconButton,
+  Button,
+  Stack,
 } from '@chakra-ui/react'
 import { FunctionComponent } from 'react'
-import { useNavigate } from 'remix'
+import { Link as RemixLink, useNavigate } from 'remix'
+
+import { ColorModeToggle } from '~/components'
 
 interface ContainerProps {
   isBackDisabled?: boolean
@@ -28,30 +38,68 @@ export const Container: FunctionComponent<ContainerProps> = ({
   }
 
   return (
-    <ChakraContainer
-      px={0}
-      borderColor="gray.600"
-      borderWidth={1}
-      borderTop={0}
-    >
-      <HStack as="header" p={5} spacing={5}>
-        {!isBackDisabled && (
-          <IconButton
-            onClick={navigateBack}
-            aria-label="Navigate Back"
-            icon={<ArrowBackIcon />}
-            borderRadius="full"
-            variant="ghost"
-          />
-        )}
-        <Heading as="h1" size="lg">
-          {headingText || 'Writter'}
-        </Heading>
+    <ChakraContainer px={0} maxWidth="720px">
+      <HStack align="flex-start">
+        <NavigationSidebar />
+
+        <Box borderColor="gray.600" borderWidth={1} borderTop={0} width="100%">
+          <HStack as="header" p={5} spacing={5}>
+            {!isBackDisabled && (
+              <IconButton
+                onClick={navigateBack}
+                aria-label="Navigate Back"
+                icon={<ArrowBackIcon />}
+                borderRadius="full"
+                variant="ghost"
+              />
+            )}
+            <Heading as="h1" size="lg">
+              {headingText || 'Writter'}
+            </Heading>
+          </HStack>
+
+          <Divider />
+
+          {children}
+        </Box>
       </HStack>
-
-      <Divider />
-
-      {children}
     </ChakraContainer>
   )
 }
+
+export const NavigationSidebar = () => (
+  <Stack p={5}>
+    <HStack spacing={5} justify="space-between">
+      <RemixLink to="/">
+        <Image
+          boxSize="50px"
+          objectFit="contain"
+          src="/android-chrome-192x192.png"
+          alt="Writter Logo"
+        />
+      </RemixLink>
+      <ColorModeToggle />
+    </HStack>
+
+    <Button as={RemixLink} to="/home" leftIcon={<TimeIcon />} variant="ghost">
+      Home
+    </Button>
+    <Button
+      as={RemixLink}
+      to="/profile"
+      leftIcon={<SettingsIcon />}
+      variant="ghost"
+    >
+      Profile
+    </Button>
+    <Button
+      as={RemixLink}
+      to="/wreet"
+      leftIcon={<EditIcon />}
+      variant="solid"
+      colorScheme="red"
+    >
+      Wreet
+    </Button>
+  </Stack>
+)
