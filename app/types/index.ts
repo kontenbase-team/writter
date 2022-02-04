80b41d2024b53cd6bfd432a51291ffedd26fced0
M Haidar Hanif <git@mhaidarhanif.com>
2022-02-05 00:49:20 +0700

    feat: wreet typing

--- app/types/index.ts
+++ app/types/index.ts
@@ -1,5 +1,14 @@
 export type TWreet = {
-  id: string
+  _id: string
   content: string
   createdAt: string
+  createdBy: TUser
+}
+
+export type TUser = {
+  _id: string
+  firstName?: string
+  lastName?: string
+  name?: string
+  handle?: string
 }
