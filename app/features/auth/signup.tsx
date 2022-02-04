0dad8aa2901c63376c588f1151cb7175dc83b5e1
M Haidar Hanif <git@mhaidarhanif.com>
2022-02-05 03:53:57 +0700

    feat+style: sign up form components

--- app/features/auth/signup.tsx
+++ app/features/auth/signup.tsx
@@ -1,4 +1,14 @@
-import { Button, Input, InputGroup, InputRightElement } from '@chakra-ui/react'
+import {
+  Button,
+  FormControl,
+  FormHelperText,
+  FormLabel,
+  Input,
+  InputGroup,
+  InputRightElement,
+  Spacer,
+  Stack,
+} from '@chakra-ui/react'
 import { FunctionComponent, useState } from 'react'
 import { Form } from 'remix'
 
@@ -10,18 +20,47 @@ export const SignUpForm: FunctionComponent<SignUpFormProps> = () => {
 
   return (
     <Form>
-      <InputGroup size="md">
-        <Input
-          pr="4.5rem"
-          type={show ? 'text' : 'password'}
-          placeholder="Enter password"
-        />
-        <InputRightElement width="4.5rem">
-          <Button h="1.75rem" size="sm" onClick={handleClickPassword}>
-            {show ? 'Hide' : 'Show'}
-          </Button>
-        </InputRightElement>
-      </InputGroup>
+      <Stack>
+        <Stack direction={['column', 'row']}>
+          <FormControl isRequired>
+            <FormLabel htmlFor="firstName">First Name</FormLabel>
+            <Input name="firstName" type="text" placeholder="Ally" />
+          </FormControl>
+
+          <FormControl isRequired>
+            <FormLabel htmlFor="lastName">Last Name</FormLabel>
+            <Input name="lastName" type="text" placeholder="Gator" />
+          </FormControl>
+        </Stack>
+
+        <FormControl isRequired>
+          <FormLabel htmlFor="email">Email address</FormLabel>
+          <Input name="email" type="email" placeholder="yourname@domain.com" />
+        </FormControl>
+
+        <FormControl isRequired>
+          <FormLabel htmlFor="password">Password</FormLabel>
+          <InputGroup size="md">
+            <Input
+              name="password"
+              pr="4.5rem"
+              type={show ? 'text' : 'password'}
+              placeholder="Enter password"
+            />
+            <InputRightElement width="4.5rem">
+              <Button h="1.75rem" size="sm" onClick={handleClickPassword}>
+                {show ? 'Hide' : 'Show'}
+              </Button>
+            </InputRightElement>
+          </InputGroup>
+        </FormControl>
+
+        <Spacer />
+
+        <Button type="submit" colorScheme="red">
+          Create Account
+        </Button>
+      </Stack>
     </Form>
   )
 }
