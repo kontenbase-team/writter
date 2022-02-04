78321885696b7da61bf0f1b5fccb7c53c5a0d662
M Haidar Hanif <git@mhaidarhanif.com>
2022-02-04 23:54:21 +0700

    feat: about page

---
+++ app/routes/about.tsx
@@ -0,0 +1,11 @@
+import { FunctionComponent } from 'react'
+
+interface AboutProps {}
+
+const About: FunctionComponent<AboutProps> = () => (
+  <div>
+    <h1>About</h1>
+  </div>
+)
+
+export default About
