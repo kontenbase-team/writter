2ce33059a19a51d44d52abe41049428f1d8fe1fa
M Haidar Hanif <git@mhaidarhanif.com>
2022-02-05 02:29:56 +0700

    feat: user profile

---
+++ app/features/user/profile.tsx
@@ -0,0 +1,23 @@
+import { Box, Avatar, Heading, Stack } from '@chakra-ui/react'
+import { FunctionComponent } from 'react'
+
+import type { TUser } from '~/types'
+import { getUserHandle, getUserName } from '~/utils'
+
+interface WreetCardProps {
+  user: TUser
+}
+
+export const UserProfile: FunctionComponent<WreetCardProps> = ({ user }) => (
+  <Box p={5}>
+    <Stack spacing={5}>
+      <Avatar size="2xl" name={getUserName(user)} />
+      <Heading as="h1" size="lg">
+        {getUserName(user)}
+      </Heading>
+      <Heading as="h2" size="md" color="gray.400">
+        @{getUserHandle(user)}
+      </Heading>
+    </Stack>
+  </Box>
+)
