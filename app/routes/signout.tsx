f28806e353be83ddc34cb1fb4d9d8d18a4839b24
M Haidar Hanif <git@mhaidarhanif.com>
2022-02-08 02:04:19 +0700

    feat: sign out loader to redirect or show user

--- app/routes/signout.tsx
+++ app/routes/signout.tsx
@@ -1,6 +1,13 @@
 import { Button, Stack, Text } from '@chakra-ui/react'
 import { FunctionComponent } from 'react'
-import { ActionFunction, Form, MetaFunction } from 'remix'
+import {
+  ActionFunction,
+  Form,
+  json,
+  LoaderFunction,
+  MetaFunction,
+  useLoaderData,
+} from 'remix'
 
 import { Container } from '~/components'
 import { authenticator } from '~/services/auth.server'
@@ -15,17 +22,30 @@ export const action: ActionFunction = async ({ request }) => {
   await authenticator.logout(request, { redirectTo: '/' })
 }
 
-const SignOut: FunctionComponent<SignOutProps> = () => (
-  <Container headingText="Sign Out">
-    <Form method="post" action="/signout">
-      <Stack p={5}>
-        <Text>Are you sure to sign out from Writter?</Text>
-        <Button type="submit" colorScheme="red">
-          Sign out
-        </Button>
-      </Stack>
-    </Form>
-  </Container>
-)
+export const loader: LoaderFunction = async ({ request }) => {
+  const user = await authenticator.isAuthenticated(request, {
+    failureRedirect: '/',
+  })
+  return json({ user })
+}
+
+const SignOut: FunctionComponent<SignOutProps> = () => {
+  const { user } = useLoaderData()
+
+  return (
+    <Container headingText="Sign Out">
+      <Form method="post" action="/signout">
+        <Stack p={5}>
+          <Text>
+            Are you sure to sign out <b>{user.email}</b> from Writter?
+          </Text>
+          <Button type="submit" colorScheme="red">
+            Sign out
+          </Button>
+        </Stack>
+      </Form>
+    </Container>
+  )
+}
 
 export default SignOut
