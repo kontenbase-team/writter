72f982edbee9d75a474537cff8fa2dcc61162af9
M Haidar Hanif <git@mhaidarhanif.com>
2022-02-08 05:57:38 +0700

    fix: get user name function

--- app/utils/user.ts
+++ app/utils/user.ts
@@ -1,16 +1,16 @@
 /* eslint-disable no-else-return */
 import type { TUser } from '~/types'
 
-export const getUserHandle = (user: TUser) => user.handle
+export const getUserHandle = (user: TUser) => user?.handle
 
 export const getUserName = (user: TUser) => {
-  if (user.firstName && user.lastName) {
-    return `${user.firstName} ${user.lastName}`
-  } else if (user.firstName) {
-    return user.firstName
-  } else if (user.lastName) {
-    return user.lastName
+  if (user?.firstName && user?.lastName) {
+    return `${user?.firstName} ${user?.lastName}`
+  } else if (user?.firstName) {
+    return user?.firstName
+  } else if (user?.lastName) {
+    return user?.lastName
   } else {
-    return `No first name`
+    return `No name`
   }
 }
