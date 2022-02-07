e16b20b68e8785a3d827c07b5f20ad711b3dacb5
M Haidar Hanif <git@mhaidarhanif.com>
2022-02-08 04:31:11 +0700

    refactor: user handle error handling

--- app/routes/$userHandle/index.tsx
+++ app/routes/$userHandle/index.tsx
@@ -1,6 +1,12 @@
 import { Box } from '@chakra-ui/react'
 import { FunctionComponent } from 'react'
-import { json, LoaderFunction, MetaFunction, useLoaderData } from 'remix'
+import {
+  json,
+  LoaderFunction,
+  MetaFunction,
+  useLoaderData,
+  useParams,
+} from 'remix'
 
 import { Container } from '~/components'
 import { UserProfile } from '~/features'
@@ -9,28 +15,36 @@ import { getUserName } from '~/utils'
 
 interface UserHandlePageProps {}
 
-export const meta: MetaFunction = ({ data }) => ({
-  title: `${getUserName(data?.user)} (@${data?.user?.handle}) / Writter`,
-})
+export const meta: MetaFunction = ({ data, params }) => {
+  if (data.user) {
+    return {
+      title: `${getUserName(data?.user)} (@${data?.user?.handle}) / Writter`,
+    }
+  }
+  return { title: `User @${params.userHandle} not found` }
+}
 
 export const loader: LoaderFunction = async ({ params }) => {
   const { data: users, error } = await kontenbaseServer.service('Users').find({
     where: { handle: params?.userHandle as string },
   })
+  const user = users?.[0]
 
   if (error) return json({ error }, { status: 404 })
+  if (!user) return json({ error: 'User not found' }, { status: 404 })
   return json({ user: users?.[0], error })
 }
 
 const UserHandlePage: FunctionComponent<UserHandlePageProps> = () => {
   const { user, error } = useLoaderData()
+  const { userHandle } = useParams()
 
   return (
-    <Container headingText={getUserName(user)}>
+    <Container headingText={user ? getUserName(user) : `@${userHandle}`}>
       {user && <UserProfile user={user} />}
       {error && (
         <Box p={3}>
-          <p>Error: User Profile {error?.message}</p>
+          <p>Error: {JSON.stringify(error, null, 2)}</p>
         </Box>
       )}
     </Container>
--- app/utils/user.ts
+++ app/utils/user.ts
@@ -1,6 +1,16 @@
+/* eslint-disable no-else-return */
 import type { TUser } from '~/types'
 
-export const getUserName = (user: TUser) =>
-  `${user?.firstName} ${user?.lastName}`
+export const getUserHandle = (user: TUser) => user.handle
 
-export const getUserHandle = (user: TUser) => user?.handle
+export const getUserName = (user: TUser) => {
+  if (user.firstName && user.lastName) {
+    return `${user.firstName} ${user.lastName}`
+  } else if (user.firstName) {
+    return user.firstName
+  } else if (user.lastName) {
+    return user.lastName
+  } else {
+    return `No first name`
+  }
+}
