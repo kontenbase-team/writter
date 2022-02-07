b8c2f33a6eecf3096f9068502fd6514fcdc2b940
M Haidar Hanif <git@mhaidarhanif.com>
2022-02-08 05:35:32 +0700

    fix: content trimmed tweet

--- app/utils/wreet.ts
+++ app/utils/wreet.ts
@@ -4,4 +4,5 @@ import { getUserHandle } from '~/utils'
 export const getWreetURL = (wreet: TWreet) =>
   `/${getUserHandle(wreet?.createdBy)}/${wreet?._id}`
 
-export const getTrimmedWreet = (content: string) => content.substring(0, 42)
+export const getTrimmedWreet = (content: string) =>
+  content?.length ? content?.substring(0, 42) : ''
