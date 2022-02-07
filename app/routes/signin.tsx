942f0b8ee3c8cca9497274f85b75b3912f321652
M Haidar Hanif <git@mhaidarhanif.com>
2022-02-07 12:15:46 +0700

    feat: sign in action function

--- app/routes/signin.tsx
+++ app/routes/signin.tsx
@@ -1,15 +1,36 @@
-import { Box, Heading, Stack, Text } from '@chakra-ui/react'
+/* eslint-disable no-console */
+import { Heading, Stack, Text } from '@chakra-ui/react'
 import { FunctionComponent } from 'react'
-import { MetaFunction } from 'remix'
+import { ActionFunction, json, MetaFunction } from 'remix'
 
 import { Container, Link } from '~/components'
 import { SignInForm } from '~/features'
+import { kontenbaseServer } from '~/lib'
 
 interface SignInProps {}
 
 export const meta: MetaFunction = () => ({
   title: 'Sign in to your Writter account',
 })
+
+/**
+ * POST /signin
+ */
+export const action: ActionFunction = async ({ request }) => {
+  const formData = await request.formData()
+
+  const { user, token, error } = await kontenbaseServer.auth.login({
+    email: formData.get('email') as string,
+    password: formData.get('password') as string,
+  })
+  console.info({ user, token, error })
+
+  if (error) {
+    console.error(error)
+    return json(null)
+  }
+  return json(null)
+}
 
 const SignUp: FunctionComponent<SignInProps> = () => (
   <Container headingText="Sign In">
