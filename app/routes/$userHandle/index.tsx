08c00f2eec4d01f11cb78348ab056abce7a8130e
M Haidar Hanif <git@mhaidarhanif.com>
2022-02-08 04:19:45 +0700

    refactor: data / users[0] to just user

--- app/routes/$userHandle/index.tsx
+++ app/routes/$userHandle/index.tsx
@@ -10,23 +10,24 @@ import { getUserName } from '~/utils'
 interface UserHandlePageProps {}
 
 export const meta: MetaFunction = ({ data }) => ({
-  title: `${getUserName(data.data)} (@${data.data.handle}) / Writter`,
+  title: `${getUserName(data?.user)} (@${data?.user?.handle}) / Writter`,
 })
 
 export const loader: LoaderFunction = async ({ params }) => {
   const { data: users, error } = await kontenbaseServer.service('Users').find({
     where: { handle: params?.userHandle as string },
   })
+
   if (error) return json({ error }, { status: 404 })
-  return json({ data: users?.[0], error })
+  return json({ user: users?.[0], error })
 }
 
 const UserHandlePage: FunctionComponent<UserHandlePageProps> = () => {
-  const { data, error } = useLoaderData()
+  const { user, error } = useLoaderData()
 
   return (
-    <Container headingText={getUserName(data)}>
-      {data && <UserProfile user={data} />}
+    <Container headingText={getUserName(user)}>
+      {user && <UserProfile user={user} />}
       {error && (
         <Box p={3}>
           <p>Error: User Profile {error?.message}</p>
