a386c49b44d04925b207a9133a1d87177a3fda0e
M Haidar Hanif <git@mhaidarhanif.com>
2022-02-08 04:47:28 +0700

    feat: profile page redirect to user.handle

--- app/routes/profile.tsx
+++ app/routes/profile.tsx
@@ -1,21 +1,16 @@
-import { Box } from '@chakra-ui/react'
-import { FunctionComponent } from 'react'
-import { MetaFunction } from 'remix'
+import { json, LoaderFunction, MetaFunction, redirect } from 'remix'
 
-import { Container } from '~/components'
-
-interface ProfileProps {}
+import { authenticator } from '~/services/auth.server'
 
 export const meta: MetaFunction = () => ({
   title: 'Redirecting to profile page...',
 })
 
-const Profile: FunctionComponent<ProfileProps> = () => (
-  <Container headingText="Profile">
-    <Box p={5}>
-      <p>Redirecting to profile page is in progress...</p>
-    </Box>
-  </Container>
-)
+export const loader: LoaderFunction = async ({ request }) => {
+  const user = await authenticator.isAuthenticated(request, {
+    failureRedirect: '/',
+  })
 
-export default Profile
+  if (user?.handle) return redirect(`/${user?.handle}`)
+  return json({ user })
+}
