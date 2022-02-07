bc10b1644da399d0b47b3d9b42e994b79bca2f7f
M Haidar Hanif <git@mhaidarhanif.com>
2022-02-08 01:42:57 +0700

    feat: sign out form

--- app/routes/signout.tsx
+++ app/routes/signout.tsx
@@ -1,8 +1,9 @@
 import { Button, Stack, Text } from '@chakra-ui/react'
 import { FunctionComponent } from 'react'
-import { MetaFunction } from 'remix'
+import { ActionFunction, Form, MetaFunction } from 'remix'
 
 import { Container } from '~/components'
+import { authenticator } from '~/services/auth.server'
 
 interface SignOutProps {}
 
@@ -10,13 +11,21 @@ export const meta: MetaFunction = () => ({
   title: 'Sign out from Writter',
 })
 
-const SignUp: FunctionComponent<SignOutProps> = () => (
+export const action: ActionFunction = async ({ request }) => {
+  await authenticator.logout(request, { redirectTo: '/' })
+}
+
+const SignOut: FunctionComponent<SignOutProps> = () => (
   <Container headingText="Sign Out">
-    <Stack p={5}>
-      <Text>Sign out from Writter</Text>
-      <Button colorScheme="red">Sign out</Button>
-    </Stack>
+    <Form method="post" action="/signout">
+      <Stack p={5}>
+        <Text>Are you sure to sign out from Writter?</Text>
+        <Button type="submit" colorScheme="red">
+          Sign out
+        </Button>
+      </Stack>
+    </Form>
   </Container>
 )
 
-export default SignUp
+export default SignOut
