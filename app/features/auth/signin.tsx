1707d9cefd25551fb965a6b947a4638cefb19e21
M Haidar Hanif <git@mhaidarhanif.com>
2022-02-07 12:21:07 +0700

    feat: sign in client side handler function

--- app/features/auth/signin.tsx
+++ app/features/auth/signin.tsx
@@ -1,3 +1,4 @@
+/* eslint-disable no-console */
 import {
   Button,
   FormControl,
@@ -5,35 +6,81 @@ import {
   Input,
   Spacer,
   Stack,
+  useToast,
 } from '@chakra-ui/react'
-import { FunctionComponent } from 'react'
-import { Form } from 'remix'
+import { FunctionComponent, SyntheticEvent, useState } from 'react'
+import { Form, useNavigate } from 'remix'
+
+import { kontenbaseClient } from '~/lib'
 
 interface SignInFormProps {}
 
-export const SignInForm: FunctionComponent<SignInFormProps> = () => (
-  <Form>
-    <Stack>
-      <FormControl isRequired>
-        <FormLabel htmlFor="email">Email address</FormLabel>
-        <Input name="email" type="email" placeholder="yourname@domain.com" />
-      </FormControl>
+export const SignInForm: FunctionComponent<SignInFormProps> = () => {
+  const navigate = useNavigate()
+  const toast = useToast()
+  const [transition, setTransition] = useState('idle')
 
-      <FormControl isRequired>
-        <FormLabel htmlFor="password">Password</FormLabel>
-        <Input
-          name="password"
-          pr="4.5rem"
-          type="password"
-          placeholder="Enter password"
-        />
-      </FormControl>
+  /**
+   * Kontenbase auth.login
+   */
+  const handleSignIn = async (event: SyntheticEvent) => {
+    try {
+      setTransition('loading')
+      event.preventDefault()
+      const target = event.target as typeof event.target & {
+        email: { value: string }
+        password: { value: string }
+      }
+      const { user, error } = await kontenbaseClient.auth.login({
+        email: target.email.value,
+        password: target.password.value,
+      })
+      if (error) {
+        toast({
+          title: 'Failed to sign in',
+          description: error.message,
+          status: 'error',
+        })
+      } else if (user) {
+        toast({ title: 'Signed in', status: 'success' })
+        navigate('/home', { replace: true })
+      }
+    } catch (error) {
+      console.error(error)
+    } finally {
+      setTransition('idle')
+    }
+  }
 
-      <Spacer />
+  return (
+    <Form method="post" action="/signin" onSubmit={handleSignIn}>
+      <Stack>
+        <FormControl isRequired>
+          <FormLabel htmlFor="email">Email address</FormLabel>
+          <Input name="email" type="email" placeholder="yourname@domain.com" />
+        </FormControl>
 
-      <Button type="submit" colorScheme="red">
-        Sign In
-      </Button>
-    </Stack>
-  </Form>
-)
+        <FormControl isRequired>
+          <FormLabel htmlFor="password">Password</FormLabel>
+          <Input
+            name="password"
+            pr="4.5rem"
+            type="password"
+            placeholder="Enter password"
+          />
+        </FormControl>
+
+        <Spacer />
+
+        <Button
+          type="submit"
+          colorScheme="red"
+          isLoading={transition === 'loading'}
+          loadingText="Signing in..."
+        >
+          Sign In
+        </Button>
+      </Stack>
+    </Form>
+  )
+}
